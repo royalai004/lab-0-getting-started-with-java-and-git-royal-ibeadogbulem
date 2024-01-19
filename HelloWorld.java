de8e89c167ae52811702db23a3b02284f74bcf9e
royal <raibeadogbulem@loyola.edu>
2024-01-19 15:10:23 -0500

    a

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,26 @@
-//HelloWorld.java
-
+// Programmers:  Royal Ibeadogbulem
+// Course:  CS 212, Dr. Nweke
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initials of the programmers in ASCII art form
+// Credits: N/A
 class HelloWorld {
+    //Prints out my first initial
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-        
+        System.out.println("_________");
+        System.out.println("   _____  \\");
+        System.out.println("|  |    \\  \\");
+        System.out.println("|  |     |  |");
+        System.out.println("|  |     |  |");
+        System.out.println("|  |    /  /");
+        System.out.println("|  |  /  /");
+        System.out.println("|  | \\  \\");
+        System.out.println("|  |   \\  \\");
+        System.out.println("|  |     \\  \\");
+        System.out.println("|__|      \\__\\");
+
     }
 }
